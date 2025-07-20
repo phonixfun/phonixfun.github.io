1a443f06461481ed77ec6fc85e73e2a1e343ff1b
Phonix Fun <phonixfun@gmail.com>
2025-07-21 01:54:44 +0200

    Github pages setup

--- src/utils/ObjectUtils.ts
+++
@@ -1,9 +0,0 @@
-export class ObjectUtils {
-    static pick<T extends Record<string, any>, K extends keyof T>(object: T, ...props: K[]): Pick<T, K> {
-        const pick = {} as Pick<T, K>;
-        for (const prop of props) {
-            pick[prop] = object[prop];
-        }
-        return pick;
-    }
-}
--- src/utils/ThreeUtils.ts
+++
@@ -1,7 +0,0 @@
-import { Mesh, Object3D } from "three";
-
-export class ThreeUtils {
-    static isMesh(object: Object3D): object is Mesh {
-        return (object as Mesh).isMesh;
-    }
-}
